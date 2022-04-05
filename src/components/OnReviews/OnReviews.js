ed21bc2e176ac46269aeee0f43e54750b88e81c7
omarfurkan <omarfurkan92@gmail.com>
2022-04-05 16:19:32 +0600

    set some css for reviews and display it on home component

--- src/components/OnReviews/OnReviews.js
+++ src/components/OnReviews/OnReviews.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import './OnReviews.css'
 
 const OnReviews = (props) => {
-    const { name, comment, rating } = props.review
+    const { name, comment, rating, img } = props.review
     return (
-        <div>
-            <div>
-                <h3>{name}</h3>
-                <p><small>{rating}</small></p>
-                <p>{comment}</p>
-            </div>
+        <div className='on-reviews-container'>
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <p><small>Rating: {rating}</small></p>
+            <p>Review: {comment}</p>
         </div>
     );
 };
