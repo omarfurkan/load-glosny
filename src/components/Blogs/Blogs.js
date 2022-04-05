51d2497160348aac1553d872e204ecf7b59627ce
omarfurkan <omarfurkan92@gmail.com>
2022-04-05 22:57:50 +0600

    in blog page 2 ques done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h3>Blog Component</h3>
+            <h1>Ques - Ans</h1>
+            <h3>Ques:1 - What is Context API?</h3>
+            <p>The Context API is a React structure that enables to exchange unique details and assits in solving prop-drilling from all levels of web application</p>
+            <p>Is is the a way fro a React app to effectively produce global variables that can be passed around</p>
+            <h3>Ques:2- Semantic Tag</h3>
+            <p>Semantic HTML tags provide information about the contents of those tags that gose beyoud just how they look on a page.</p>
+            <p>A sementic element clearly describes its meaining to both the browser and the developer. Exapmple of semantic elements: form, table article etc.</p>
         </div>
     );
 };
