99397aa49e894b044651891afcedf013b259aa88
omarfurkan <omarfurkan92@gmail.com>
2022-04-05 15:19:34 +0600

    custom hook made for customer reviews

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css'
-import product from '../../images/glosny-product.jpg'
+import product from '../../images/glosny-product.jpg';
+import Reviews from '../Reviews/Reviews';
 
 const Home = () => {
     return (
@@ -19,6 +20,7 @@ const Home = () => {
 
             <div className='customer-container'>
                 <h2>Customer Reviews</h2>
+                <Reviews></Reviews>
             </div>
         </di>
     );
